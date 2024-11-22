cc95ad6077f6fc72a7fdce26f2f5dfd259338d38
gkdl4239 <gkdl4239@gmail.com>
2024-11-22 11:06:07 +0900

    feat: 장바구니 클래스 캡슐화 및 getter setter 구현

--- src/Orderitem.java
+++ src/Orderitem.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Orderitem {
+    private List<MenuItem> orderList = new ArrayList<>();
 
+    public void setOrderList(MenuItem menu){
+        this.orderList.add(menu);
+    }
 
+    public List<MenuItem> getOrderList(){
+        return this.orderList;
+    }
 }
