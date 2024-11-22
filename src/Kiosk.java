1916e99eb3f1876945a9820cf366e1e388754e62
gkdl4239 <gkdl4239@gmail.com>
2024-11-22 13:11:55 +0900

    feat: 장바구니 및 결제 UI Kiosk에 추가

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -3,6 +3,7 @@ import java.util.List;
 public class Kiosk {
     private final List<Menu> menuList;
 
+    Orderitem orderList = new Orderitem();
     InputMenu num = new InputMenu();
 
     public Kiosk(List<Menu> menuList){
@@ -15,26 +16,62 @@ public class Kiosk {
                 System.out.println(i + ". "+menuList.get(i-1).getCategoryName());
             }
             System.out.println("0. 종료");
-            int choice = num.menuNumber();
 
-            if(choice == 0){
+            int j = menuList.size();
+            if(!orderList.getOrderList().isEmpty()) {
+                System.out.println();
+                System.out.println("[ ORDER MENU ]");
+                System.out.println(j + ". Orders");
+                System.out.println((j + 1) + ". Cancel");
+            }
+
+            int order = num.menuNumber();
+
+            if (order == j) {
+                System.out.println("아래와 같이 주문 하시겠습니까?");
+                System.out.println();
+                System.out.println("[ Orders ]");
+                for (MenuItem orders : orderList.getOrderList()) {
+                    System.out.println(orders.getName() + "\t" + "| W " + orders.getPrice() + " | " + orders.getExp());
+                }
+
+                System.out.println();
+                System.out.println("[ Total ]");
+                System.out.println("W " + orderList.getTotalPrice());
+
+                System.out.println();
+                System.out.println("1. 주문 \t 2. 메뉴판");
+                int check = num.menuNumber();
+                if (check == 1) {
+                    System.out.println("주문이 완료되었습니다. 금액은 W " + orderList.getTotalPrice() + " 입니다.");
+                    orderList.removeOrder();
+                    continue;
+                } else if (check == 2) {
+                    continue;
+                }
+            } else if (order == j + 1) {
+                orderList.removeOrder();
+                continue;
+            }
+
+            System.out.println();
+
+            if (order == 0) {
                 break;
-            }else if (choice>menuList.size()){
+            } else if (order > menuList.size()) {
                 System.out.println("유효한 메뉴를 입력하세요");
                 continue;
             }
 
-            Menu chosenMenu = menuList.get(choice-1);
+            Menu chosenMenu = menuList.get(order - 1);
             int n = chosenMenu.showItem();
-            if(n!=-1){
-                chosenMenu.getItem(n);
+            if (n != -1) {
+                MenuItem selctedItem = chosenMenu.getItem(n);
+                orderList.setOrderList(selctedItem);
             }
         }
+
     }
-
-
-
-
 }
 
 
