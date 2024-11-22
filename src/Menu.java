536d1a9b0e42c17de04fb3befa9a07b13f8a5c31
gkdl4239 <gkdl4239@gmail.com>
2024-11-22 11:05:27 +0900

    fix: 장바구니 메뉴 추가로 인한 로직 변경

--- src/Menu.java
+++ src/Menu.java
@@ -4,6 +4,7 @@ public class Menu {
     private final List<MenuItem> itemList = new ArrayList<>();
     private final String categoryName;
 
+    Orderitem orderList = new Orderitem();
     InputMenu choice = new InputMenu();
 
     Menu(String categoryName){
@@ -33,9 +34,25 @@ public class Menu {
         return num;
     }
 
-    public void getItem(int choice){
-        MenuItem m = itemList.get(choice-1);
+    public void getItem(int menuChoice){
+        MenuItem m = itemList.get(menuChoice-1);
         System.out.println("선택한 메뉴: "+m.getName()+" | W "+m.getPrice()+" | "+m.getExp());
+        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+        System.out.println("1. 확인\t 2. 취소");
+        int wannaAdd = choice.menuNumber();
+
+        if(wannaAdd==1){
+            orderList.setOrderList(m);
+        }
+
+        else if(wannaAdd==2){
+
+        }
+
+        else{
+            System.out.println("유효한 번호를 다시 선택해주세요");
+            getItem(menuChoice);
+        }
     }
 }
 
