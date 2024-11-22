a92b287cc775cf98ca3507aafe55a57f1b1ffe58
gkdl4239 <gkdl4239@gmail.com>
2024-11-22 14:15:57 +0900

    feat: 매핑 값 및 메서드 추가

--- src/Discount.java
+++ src/Discount.java
@@ -1,17 +1,28 @@
 public enum Discount {
-    NATIONALMERIT(price -> price *0.8),
-    SOLDIER(price->price*0.9),
-    STUDENT(price->price*0.95),
-    CIVILIAN(price->price);
+    NATIONALMERIT(1,price -> price *0.8),
+    SOLDIER(2,price->price*0.9),
+    STUDENT(3,price->price*0.95),
+    CIVILIAN(4,price->price);
 
+    private final int code;
     private final DiscountPolicy policy;
 
-    Discount(DiscountPolicy policy){
+    Discount(int code,DiscountPolicy policy){
+        this.code = code;
         this.policy = policy;
     }
 
     public double apply(double price){
         return policy.calculate(price);
+    }
+
+    public static Discount fromCode(int code){
+        for(Discount discount : values()){
+            if(discount.code == code){
+                return discount;
+            }
+        }
+        throw new IllegalArgumentException("유효하지 않는 번호입니다");
     }
 
     interface DiscountPolicy{
