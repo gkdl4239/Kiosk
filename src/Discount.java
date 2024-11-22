7ee6b36319a47620f667ee74249a03117fee44d7
gkdl4239 <gkdl4239@gmail.com>
2024-11-22 14:00:48 +0900

    feat: 할인율 정의 및 메서드 추가

--- src/Discount.java
+++ src/Discount.java
@@ -1,2 +1,20 @@
 public enum Discount {
+    NATIONALMERIT(price -> price *0.8),
+    SOLDIER(price->price*0.9),
+    STUDENT(price->price*0.95),
+    CIVILIAN(price->price);
+
+    private final DiscountPolicy policy;
+
+    Discount(DiscountPolicy policy){
+        this.policy = policy;
+    }
+
+    public double apply(double price){
+        return policy.calculate(price);
+    }
+
+    interface DiscountPolicy{
+        double calculate(double price);
+    }
 }
