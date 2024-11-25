4a626264559b46cfe709b995636b7a3d071d99d9
gkdl4239 <gkdl4239@gmail.com>
2024-11-25 17:26:50 +0900

    feat: 예외 처리 추가

--- src/Challenge/Check.java
+++ src/Challenge/Check.java
@@ -5,4 +5,11 @@ public class Check {
     public boolean isOrderListEmpty(OrderList orderList){
             return !orderList.getOrderList().isEmpty();
     }
+
+    public void isValidOrder(OrderList orderList,int mainInput,int orderMenuNumber){
+        if(orderList.getOrderList().isEmpty() && (mainInput == orderMenuNumber || mainInput == orderMenuNumber+1)){
+            throw new InvalidMenuSelectionException();
+        }
+    }
 }
+
