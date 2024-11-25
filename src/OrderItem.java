1c18111ac84515e51297ac9f81b45df1c2139420
gkdl4239 <gkdl4239@gmail.com>
2024-11-25 10:15:00 +0900

    refactor: 클래스이름 컨벤션 변경

--- src/OrderItem.java
+++ src/OrderItem.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Orderitem {
+public class OrderItem {
     private final List<MenuItem> orderList = new ArrayList<>();
 
     public void setOrderList(MenuItem menu){
