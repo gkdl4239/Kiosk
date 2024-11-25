de00be5fca4b1fd0e36d916465c56acc52e9b0b5
gkdl4239 <gkdl4239@gmail.com>
2024-11-25 11:26:51 +0900

    refactor: 참조하는 클래스 이름 변경

--- src/Check.java
+++ src/Check.java
@@ -2,11 +2,11 @@ import java.util.List;
 
 public class Check {
 
-    public boolean isValidMenu(int mainInput, List<Menu>menuList, OrderItem orderList){
+    public boolean isValidMenu(int mainInput, List<Menu>menuList, OrderList orderList){
         return (mainInput>=menuList.size() && orderList.getOrderList().isEmpty()) || (mainInput > menuList.size()+2);
     }
 
-    public boolean isOrderListEmpty(OrderItem orderList){
+    public boolean isOrderListEmpty(OrderList orderList){
         return !orderList.getOrderList().isEmpty();
     }
 }
