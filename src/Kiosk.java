0f54813707f1efb95957ef9187c7e6ecc82553b8
gkdl4239 <gkdl4239@gmail.com>
2024-11-25 10:40:26 +0900

    refactor: 주석 추가

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,5 +1,4 @@
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class Kiosk {
     private final List<Menu> menuList;
@@ -17,40 +16,53 @@ public class Kiosk {
 
     public void start() {
         while(true){
+            // MAIN MENU 출력
             print.printMainMenu(menuList);
 
+            // 장바구니에 메뉴가 들어있으면 ORDER MENU 출력
             if(check.isOrderListEmpty(orderList)) {
                 print.printOrderMenu(orderMenuNumber);
             }
 
             int mainInput = num.menuNumber();
 
-            check.isValidMenu(mainInput,menuList,orderList);
+            // 범위를 벗어나는 번호 입력시 예외 처리
+            if(check.isValidMenu(mainInput,menuList,orderList)){
+                throw new IllegalArgumentException("유효하지 않은 번호입니다!");
+            }
 
+            // 0 입력시 종료
             if (mainInput == 0) {
                 break;
             }
 
+            // 메인 메뉴 선택 시 처리
             else if(mainInput > 0 && mainInput <= menuList.size()){
                 Menu chosenMenu = menuList.get(mainInput - 1);
                 int n = chosenMenu.showItem();
+
+                // 메뉴 아이템 선택 시 처리
                 if(n!=-1) {
                     MenuItem selectedItem = chosenMenu.getItem(n);
+
+                    // 장바구니에 추가했다면 리스트에도 추가
                     if (selectedItem != null) {
                         orderList.setOrderList(selectedItem);
                     }
                 }
             }
 
+            // 주문 선택 시 처리
             else if (mainInput == orderMenuNumber) {
                 print.printTotalOrder(orderList);
 
                 int totalCheck = num.menuNumber();
 
-
+                // 주문 확정 시 할인 선택
                 if (totalCheck == 1) {
                     print.printDiscountChoice();
 
+                    // Enum을 이용한 할인율 로직
                     int discountCheck = num.menuNumber();
                     double originalPrice = orderList.getTotalPrice();
                     Discount discount = Discount.fromCode(discountCheck);
@@ -62,6 +74,7 @@ public class Kiosk {
                 }
             }
 
+            // 주문 초기화
             else if (mainInput == orderMenuNumber + 1) {
                 orderList.removeOrder();
             }
