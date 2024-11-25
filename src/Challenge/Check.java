b9d13d9a23db49c1697a1ef048a8effd1f144e33
gkdl4239 <gkdl4239@gmail.com>
2024-11-25 17:05:16 +0900

    refactor: 필요없는 메서드 삭제

--- src/Challenge/Check.java
+++ src/Challenge/Check.java
@@ -1,14 +1,8 @@
 package Challenge;
 
-import java.util.List;
 
 public class Check {
-
-    public boolean isValidMenu(int mainInput, List<Menu>menuList, OrderList orderList){
-        return (mainInput>menuList.size() && orderList.getOrderList().isEmpty()) || (mainInput > menuList.size()+2);
-    }
-
     public boolean isOrderListEmpty(OrderList orderList){
-        return !orderList.getOrderList().isEmpty();
+            return !orderList.getOrderList().isEmpty();
     }
 }
