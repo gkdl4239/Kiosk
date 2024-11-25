4d7e3ba2dc652d5381bb91703cab071f8c26870f
gkdl4239 <gkdl4239@gmail.com>
2024-11-25 15:25:58 +0900

    refactor: 클래스 책임 분리

--- src/Challenge/Menu.java
+++ src/Challenge/Menu.java
@@ -22,40 +22,16 @@ public class Menu {
         this.itemList.add(m);
     }
 
-    public int showItem(){
+    public void showItem(){
         print.printMenuItemList(itemList,this.categoryName);
-        int num = choice.menuNumber();
+    }
 
-        // 뒤로가기 처리
-        if(num==0){
-            return -1;
-        }
-
-        // 선택한 메뉴 번호 반환
-        return num;
+    public List<MenuItem> getItemList(){
+        return this.itemList;
     }
 
     public MenuItem getItem(int menuChoice){
-        MenuItem selected = itemList.get(menuChoice-1);
-        print.printWannaAdd(selected);
-
-        int wannaAdd = choice.menuNumber();
-
-        if(wannaAdd==1){
-            System.out.println(selected.getName()+" 이 장바구니에 추가되었습니다.");
-            return selected;
-        }
-
-        else if(wannaAdd==2){
-            System.out.println("취소되었습니다.");
-            return null;
-        }
-
-        else{
-            System.out.println("유효한 번호를 다시 선택해주세요");
-            return getItem(menuChoice);
-        }
-
+        return itemList.get(menuChoice-1);
     }
 }
 
