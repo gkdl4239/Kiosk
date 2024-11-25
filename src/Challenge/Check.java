a274e358ca9063d8f528594f4f7b2cad757bcf33
gkdl4239 <gkdl4239@gmail.com>
2024-11-25 15:26:42 +0900

    fix: 잘못된 조건 수정

--- src/Challenge/Check.java
+++ src/Challenge/Check.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Check {
 
     public boolean isValidMenu(int mainInput, List<Menu>menuList, OrderList orderList){
-        return (mainInput>=menuList.size() && orderList.getOrderList().isEmpty()) || (mainInput > menuList.size()+2);
+        return (mainInput>menuList.size() && orderList.getOrderList().isEmpty()) || (mainInput > menuList.size()+2);
     }
 
     public boolean isOrderListEmpty(OrderList orderList){
