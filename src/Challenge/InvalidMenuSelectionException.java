7f7626ad3c65079a4eadfcafea2f9a758df96305
gkdl4239 <gkdl4239@gmail.com>
2024-11-26 23:10:34 +0900

    refactor: 예외 구문 수정

--- src/Challenge/InvalidMenuSelectionException.java
+++ src/Challenge/InvalidMenuSelectionException.java
@@ -2,6 +2,6 @@ package Challenge;
 
 public class InvalidMenuSelectionException extends IllegalArgumentException {
     public InvalidMenuSelectionException() {
-        super("유효하지 않은 번호 입니다.");
+        super("유효하지 않은 입력 입니다.");
     }
 }
